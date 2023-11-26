c27dd367200322728080575240775d9b3cc65d09
remonhasanapu <remon.hasan@technovista.com.bd>
2023-11-26 10:50:27 +0600

    ❌ Delete logo and contact

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -237,7 +237,6 @@
     <!-- footer section start -->
     <div class="footer_section layout_padding">
         <div class="container">
-            <div class="footer_logo"><a href="index.html"><img src="images/footer-logo.png"></a></div>
             <div class="input_bt">
                 <input type="text" class="mail_bt" placeholder="Your Email" name="Your Email">
                 <span class="subscribe_bt" id="basic-addon2"><a href="#">Subscribe</a></span>
@@ -251,14 +250,13 @@
                     <li><a href="#">Customer Service</a></li>
                 </ul>
             </div>
-            <div class="location_main">Help Line Number : <a href="#">+1 1800 1200 1200</a></div>
         </div>
     </div>
     <!-- footer section end -->
     <!-- copyright section start -->
     <div class="copyright_section">
         <div class="container">
-            <p class="copyright_text">© 2023 All Rights Reserved. Design by <a href="https://html.design">
+            <p class="copyright_text">© 2023 All Rights Reserved. Design by <a href="https://github.com/Remonhasan">
                     Remon Hasan</a></p>
         </div>
     </div>
